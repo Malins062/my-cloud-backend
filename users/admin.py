95d2f9bc3d8bfa5be68a79a36eaf36eaed0ad125
Alexei Malinovskiy <6328673@gmail.com>
2024-02-21 10:30:10 +0300

    fix: refactoring

--- users/admin.py
+++ users/admin.py
@@ -12,13 +12,13 @@ admin.site.unregister(Group)
 class UserAdmin(UserAdmin):
     change_user_password_template = None
     fieldsets = (
-        ('Идентификаторы пользователя', {'fields': ('email', 'username',)}),
-        ('Личная информация', {'fields': ('first_name', 'last_name',)}),
-        ('Параметры доступа', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        ('Идентификаторы пользователя', {'fields': ('email', 'username', )}),
+        ('Личная информация', {'fields': ('first_name', 'last_name', )}),
+        ('Параметры доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', )}),
         # ('Параметры доступа', {
         #     'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         # }),
-        ('Важные даты', {'fields': ('date_joined', 'last_login',)}),
+        ('Важные даты', {'fields': ('date_joined', 'last_login', )}),
     )
     # add_fieldsets = (
     #     (None, {
@@ -29,8 +29,8 @@ class UserAdmin(UserAdmin):
     list_display = ('id', 'username', 'full_name', 'email', )
 
     list_display_links = ('id', 'username', 'full_name',)
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number',)
-    ordering = ('id',)
-    filter_horizontal = ('groups', 'user_permissions',)
-    readonly_fields = ('last_login', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', )
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', )
+    ordering = ('id', )
+    filter_horizontal = ('groups', 'user_permissions', )
+    readonly_fields = ('last_login', 'date_joined', )
